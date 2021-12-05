cf3bf98dbec5d1ebfe499899c971f007cf0551e0
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-12-06 00:54:23 +0300

    Add Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {
+  AiFillGithub,
+  AiFillMediumCircle,
+  AiFillLinkedin,
+} from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+90-531-299-7121'>+90 531 299 7121</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:dogancanulgu@gmail.com'>
+            dogancanulgu@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/dogancanulgu' target='_blank'>
+            <AiFillGithub size='3rem' />
+          </SocialIcons>
+          <SocialIcons
+            href='https://www.linkedin.com/in/dogancanulgu/'
+            target='_blank'
+          >
+            <AiFillLinkedin size='3rem' />
+          </SocialIcons>
+          <SocialIcons href='https://medium.com/@dogancanulgu' target='_blank'>
+            <AiFillMediumCircle size='3rem' />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
